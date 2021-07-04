3b7cd3fbbaa35230a43849a2571c93ab9eed79ca
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-07-04 21:36:57 +0300

    Add files via upload

---
+++ MySimpleStack/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace MySimpleStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack mySimpleStack = new Stack();
+            mySimpleStack.Push("Hello");
+            mySimpleStack.Push("World");
+            mySimpleStack.Push("!");
+
+            Console.WriteLine("mySimpleStack:");
+            Console.WriteLine("\t* Count:    {0}", mySimpleStack.Count);
+            Console.Write("\t* Values:");
+
+            StackPrinter(mySimpleStack);
+        }
+        public static void StackPrinter(IEnumerable mySimpleStack)
+        {
+            foreach (var element in mySimpleStack)
+            {
+                Console.Write("    {0}", element);
+            }
+
+            Console.WriteLine();
+        }
+    
+    }
+}
