1ab3531a4d6cb3758a1ebba7c3bd8f8062468816
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-07-10 17:51:40 +0300

    Add files via upload

---
+++ GeneratingFibonacciSeries/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace GeneratingFibonacciSeries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fibonacciSeriesLength = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < fibonacciSeriesLength; i++)
+            {
+                Console.Write("{0} ", FibonacciNumberGenerator(i));
+            }
+        }
+
+        public static int FibonacciNumberGenerator(int n)
+        {
+            int firstNumber = 0;
+            int secondNumber = 1;
+            int result = 0;
+
+            if (n == 0)
+            {
+                return 0;
+            }
+
+            if (n == 1)
+            {
+                return 1;
+            }
+
+            for (int i = 2; i <= n; i++)
+            {
+                result = firstNumber + secondNumber;
+                firstNumber = secondNumber;
+                secondNumber = result;
+            }
+            return result;
+        }
+    }
+}
