74e4a7f2e9a1c045a9e4fb821b970d7056d4974c
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-07-17 00:53:58 +0300

    Add files via upload

---
+++ CustomHashSetWithDuplicates/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomHashSetWithDuplicates
+{
+    public class Employee : IEquatable<Employee>
+    {
+        public int Emp_Id { get; set; }
+
+        public string Emp_name { get; set; }
+
+        public string Dept_name { get; set; }
+
+        public bool Equals(Employee employee)
+        {
+            return Emp_Id.Equals(employee.Emp_Id);
+        }
+        public override int GetHashCode()
+        {
+            return Emp_Id.GetHashCode();
+        }
+    }
+}
---
+++ CustomHashSetWithDuplicates/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomHashSetWithDuplicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Employee> staff = new HashSet<Employee> 
+            {
+                new Employee { Emp_Id = 1, Emp_name = "Petko", Dept_name = "R&D" },
+                new Employee { Emp_Id = 1, Emp_name = "Petko", Dept_name = "R&D" },
+                new Employee { Emp_Id = 3, Emp_name = "Misho", Dept_name = "R&D" },
+                new Employee { Emp_Id = 4, Emp_name = "Gogi", Dept_name = "R&D" },
+                new Employee { Emp_Id = 5, Emp_name = "Dimo", Dept_name = "R&D" }
+            };
+
+            Console.WriteLine("{0} {1} {2}", "Id", "Employee", "Deptment");
+
+            foreach (Employee employee in staff)
+            { 
+                Console.WriteLine("{0}  {1}        {2}", employee.Emp_Id, employee.Emp_name, employee.Dept_name);
+            }
+            
+            
+        }
+    }
+    
+}
