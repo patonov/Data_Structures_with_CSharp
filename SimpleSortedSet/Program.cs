70a1b0680532727e2d0dbf808cb4523634b82b3f
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-07-18 22:34:14 +0300

    Add files via upload

---
+++ SimpleSortedSet/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleSortedSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<int> initialSortedSet = new SortedSet<int>();
+
+            for (int i = 0; i <= 10; i++)
+            {
+                initialSortedSet.Add(i);
+            }
+            Console.WriteLine("The Initial SortedSet includes:");
+
+            foreach (var element in initialSortedSet)
+            {
+                Console.Write(element + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("The Initial SortedSet contains {0} elements.", initialSortedSet.Count);
+            Console.WriteLine();
+
+            if (initialSortedSet.Contains(2) == true)
+            {
+                Console.WriteLine("The number 2 is available.");
+            }
+            else
+            {
+                Console.WriteLine("The number 2 is NOT available.");
+            }
+            Console.WriteLine();
+
+            initialSortedSet.RemoveWhere(n => n % 2 == 0);
+
+            Console.Write("The Initial SortedSet contains {0} elements after removing the even numbers.", initialSortedSet.Count);
+            Console.Write("These are: ");
+
+            foreach (var element in initialSortedSet)
+            {
+                Console.Write(element + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            initialSortedSet.Clear();
+
+            Console.WriteLine("The Initial SortedSet contains {0} elements after clearing.", initialSortedSet.Count);
+        }
+    }
+}
