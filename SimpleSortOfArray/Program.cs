e32bfcb5d136dd5765110d4ba4e6794988d24cf1
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-07-22 18:37:03 +0300

    Add files via upload

---
+++ SimpleSortOfArray/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleSortOfArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Console.WriteLine("Before Sorting:");
+            foreach (var n in nums)
+            {
+                Console.WriteLine(n);
+
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("After Sorting:");
+            Array.Sort(nums);
+
+            foreach (var n in nums)
+            {
+                Console.WriteLine(n);
+
+            }
+            Console.WriteLine();
+        }
+    }
+}
