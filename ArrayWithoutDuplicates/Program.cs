deef44e45bd925202ac336bd1c0774195933631e
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-07-23 19:05:03 +0300

    Add files via upload

---
+++ ArrayWithoutDuplicates/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayWithoutDuplicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArr = { 5, 5, 5, 12, 41, 55, 41, 11, 5 };
+            int[] outputArr = inputArr.Distinct().ToArray();
+            Array.Sort(outputArr);
+
+            foreach (var n in outputArr)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
