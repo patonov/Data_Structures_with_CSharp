ac8689853edbc6efb838e487d188ed269cbe15af
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-09-19 16:52:30 +0300

    Add files via upload

---
+++ NewHashtableTrial/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace NewHashtableTrial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable simpleHashtable = new Hashtable();
+
+
+            for (int i = 1; i < 6; i++)
+            {
+                simpleHashtable.Add(i, "Mathematics" + " " + i);
+            }
+
+            //Numbers and topics will be printed in reverse order.
+
+            foreach (DictionaryEntry kvp in simpleHashtable)
+            {
+                Console.WriteLine($"Number: {kvp.Key} -> Topic: {kvp.Value}");
+            }
+
+            
+        }
+    }
+}
