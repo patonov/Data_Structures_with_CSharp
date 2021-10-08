a1565ffc8dbf1ecc79d09656cf728f54d148f49a
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-10-08 21:36:10 +0300

    Add files via upload

---
+++ MySimpleStaticList/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MySimpleStaticList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StaticList.RecordValue("Tupak");
+            StaticList.RecordValue("Glupak");
+            StaticList.RecordValue("Tikva");
+                        
+            StaticList.DisplayAllValues();
+        }
+    }
+}
---
+++ MySimpleStaticList/StaticList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MySimpleStaticList
+{
+    public class StaticList
+    {
+        static List<string> myList;
+
+        static StaticList()
+        {
+            myList = new List<string>();
+        }
+
+        public static void RecordValue(string v)
+        {
+            myList.Add(v);
+        }
+
+        public static void DisplayAllValues()
+        {
+            foreach (string val in myList)
+            {
+                Console.WriteLine(val);
+            }
+        }
+    }
+}
