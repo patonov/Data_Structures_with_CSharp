f3298f150e94c00df6f7bff5f1ea5387bdccab22
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-10-12 22:59:11 +0300

    Add files via upload

---
+++ MyStaticArrayDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MyStaticArrayDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string breed = Console.ReadLine();
+
+            Console.WriteLine(IsBreedAmongDefaultOnes(breed));
+        
+        }
+
+        public static bool IsBreedAmongDefaultOnes(string breed)
+        {
+            bool IsThere = false;
+
+            foreach (string dog in Dog.Breeds)
+            {
+                if (breed == dog)
+                {
+                    IsThere = true;
+                }
+            }
+
+            return IsThere;
+        }
+    }
+
+    public static class Dog
+    {
+        static string[] defaultBreeds = new string[] { "minik", "russian layka", "pomiyar" };
+
+        public static string[] Breeds => defaultBreeds;
+        
+    }
+}
