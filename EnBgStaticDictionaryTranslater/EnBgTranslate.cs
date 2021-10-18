a25736d8dc0d4984aff81ce65d0cd86d91e61708
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-10-18 19:09:49 +0300

    Add files via upload

---
+++ EnBgStaticDictionaryTranslater/EnBgTranslate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnBgStaticDictionaryTranslater
+{
+    public static class EnBgTranslate
+    {
+        static Dictionary<string, string> dict = new Dictionary<string, string>() { 
+        {"translate", "превеждам"},
+        {"image", "изображение"},
+        {"picture", "картина"},
+        {"great", "велик"},
+        {"option", "опция"},
+        {"car", "кола"},
+        {"homosexual", "хомосексуалист"},
+        {"inflation", "инфлация"},
+        {"multiplier", "мултипликатор"},
+        {"fiscal", "фискален"},
+        {"digital", "цифров"}
+    };
+
+        public static string Translaing(string word)
+        {
+            string result = string.Empty;
+
+            if (dict.TryGetValue(word, out result))
+            {
+                return result;
+            }
+            else
+            {
+                return "out of our dictionary.";
+            }
+
+        }
+    }
+}
---
+++ EnBgStaticDictionaryTranslater/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EnBgStaticDictionaryTranslater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            var result = EnBgTranslate.Translaing(input);
+            Console.WriteLine("Bulgarian word for {0} is: {1}", input, result);
+        }
+    }
+}
