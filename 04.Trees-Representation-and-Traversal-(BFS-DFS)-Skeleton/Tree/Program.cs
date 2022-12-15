59ac3800ffe7dc9f09067f877b9bdbbf795326bf
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2022-12-15 17:06:18 +0200

    Add files via upload

---
+++ 04.Trees-Representation-and-Traversal-(BFS-DFS)-Skeleton/Tree/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Tree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Tree<int> tree = new Tree<int>(7,
+                        new Tree<int>(19,
+                                new Tree<int>(1),
+                                new Tree<int>(12),
+                                new Tree<int>(31)),
+                        new Tree<int>(21),
+                        new Tree<int>(14,
+                                new Tree<int>(23),
+                                new Tree<int>(6)));
+
+
+            //tree.AddChild(7, new Tree<int>(-1, new Tree<int>(-2), new Tree<int>(-3)));
+
+            //tree.RemoveNode(14);
+
+            //tree.Swap(19, 14);
+
+            Console.WriteLine(string.Join(", ", tree.OrderBfs()));
+            Console.WriteLine(string.Join(", ", tree.OrderDfs()));
+        }
+    }
+}
