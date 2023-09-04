2a75b8960c11f1a0f85e0f083056e3364f8ff302
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2023-09-04 18:05:00 +0300

    Add files via upload

---
+++ Problem02.Stack/Problem02.Stack/MyStack.cs
@@ -0,0 +1,103 @@
+﻿namespace Problem02.Stack
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class MyStack<T> : IAbstractStack<T>
+    {
+        private class Node
+        {
+            public T Element { get; set; }
+            public Node Next { get; set; }
+
+            public Node(T element, Node next)
+            {
+                this.Element = element;
+                this.Next = next;
+            }
+
+            public Node(T element)
+            {
+                this.Element = element;
+            }
+        }
+
+        private Node top;
+
+        public MyStack()
+        {
+            this.top = null;
+            this.Count = 0;
+        }
+
+        public int Count { get; private set; }
+
+        public void Push(T item)
+        {
+            Node node = new Node(item);
+            node.Next = this.top;
+            this.top = node;
+            this.Count++;
+        }
+
+        public T Pop()
+        {
+            this.ValidateNotEmpty();
+
+            T item = this.top.Element;
+            this.top = this.top.Next;
+            this.Count--;
+
+            return item;
+        }
+
+        public T Peek()
+        {
+            this.ValidateNotEmpty();
+
+            return this.top.Element;
+        }
+
+        public bool Contains(T item)
+        {
+            Node current = this.top;
+
+            while (current != null)
+            {
+                if (current.Element.Equals(item))
+                {
+                    return true;
+                }
+
+                current = current.Next;
+            }
+
+            return false;
+        }
+
+        private void ValidateNotEmpty()
+        {
+            if (this.top == null)
+            {
+                throw new InvalidOperationException("The stack is empty!");
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node current = this.top;
+
+            while (current != null)
+            {
+                yield return current.Element;
+                current = current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ Problem02.Stack/Problem02.Stack/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Problem02.Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack<int> ints = new MyStack<int>();
+            int one = 1;
+            int two = 2;
+            int three = 3;
+
+            ints.Push(three);
+            ints.Push(two);
+            ints.Push(one);
+
+            Console.WriteLine(string.Join(" ", ints));
+            Console.WriteLine(ints.Peek());
+            Console.WriteLine(ints.Count());
+
+            Console.WriteLine(ints.Pop());
+            Console.WriteLine(ints.Pop());
+            Console.WriteLine(ints.Pop());
+            
+
+
+        }
+    }
+}
